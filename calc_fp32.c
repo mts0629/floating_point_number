d8b68ada1a3110768f09b902ad07a103b7605303
mts0629 <soratobi93@gmail.com>
2025-04-20 23:21:47 +0900

    Deal with overflow

--- calc_fp32.c
+++ calc_fp32.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
+#include <float.h>
 
 #include "fp32.h"
 
 int main(void) {
-    // 1000: 0x447a0000, sign=0, exp=136, mantissa=0x7a0000
+    // 1000: sign=0, exp=136, mantissa=0x7a0000
     Binary32 a = fp32_cvt_to_binary32(1000);
-    // 0.5: 0x3f000000, sign=0, exp=126, mantissa=0x0
+    // 0.5: sign=0, exp=126, mantissa=0x0
     Binary32 b = fp32_cvt_to_binary32(0.5);
-
-    // 1000.5: 0x447a2a00, sign=0, exp=136, mantissa=0x7a2000
+    // 1000 + 0.5 = 1000.5: sign=0, exp=136, mantissa=0x7a2000
     Binary32 c = fp32_add_binary32(a, b);
     printf("%f + %f = %f (sign: %u, exp: %u, mantissa: 0x%x)\n",
         fp32_cvt_to_float(a), fp32_cvt_to_float(b), fp32_cvt_to_float(c),
         c.sign, c.exp, c.mantissa
     );
 
+    // Carry
     a = fp32_cvt_to_binary32(1000);
     b = fp32_cvt_to_binary32(1000);
+    // 1000 + 1000 = 2000: sign=0, exp=137, mantissa=0x7a0000
+    c = fp32_add_binary32(a, b);
+    printf("%f + %f = %f (sign: %u, exp: %u, mantissa: 0x%x)\n",
+        fp32_cvt_to_float(a), fp32_cvt_to_float(b), fp32_cvt_to_float(c),
+        c.sign, c.exp, c.mantissa
+    );
 
-    // 2000: 0x44fa0000, sign=0, exp=137, mantissa=0x7a0000
+    // Overflow: return +-inf
+    a = fp32_cvt_to_binary32(FLT_MAX);
+    b = fp32_cvt_to_binary32(1.70141183e+38);
     c = fp32_add_binary32(a, b);
     printf("%f + %f = %f (sign: %u, exp: %u, mantissa: 0x%x)\n",
         fp32_cvt_to_float(a), fp32_cvt_to_float(b), fp32_cvt_to_float(c),
--- src/fp32.c
+++ src/fp32.c
@@ -21,9 +21,9 @@ float fp32_cvt_to_float(const Binary32 bin32) {
 }
 
 Binary32 fp32_add_binary32(const Binary32 a, const Binary32 b) {
-    // Signed mantissas...                with a hidden bit
-    int32_t a_man = (int32_t)a.mantissa | (0x1 << 23) | ((a.sign & 0x1) << 31);
-    int32_t b_man = (int32_t)b.mantissa | (0x1 << 23) | ((b.sign & 0x1) << 31);
+    // Signed mantissas...                              with a hidden bit
+    int32_t a_man = (int32_t)((a.mantissa & 0x7fffff) | (0x1 << 23) | ((a.sign & 0x1) << 31));
+    int32_t b_man = (int32_t)((b.mantissa & 0x7fffff) | (0x1 << 23) | ((b.sign & 0x1) << 31));
 
     // Adjust exp. scale to a large one
     uint8_t sum_exp = a.exp;
@@ -37,20 +37,29 @@ Binary32 fp32_add_binary32(const Binary32 a, const Binary32 b) {
     // Add mantissas
     int32_t sum_man = a_man + b_man;
 
+    // Signature bit
+    uint8_t sign = ((uint32_t)sum_man & 0x80000000) >> 31;
+
     // Normalize summed mantissa: scale to make MSB (b23) to 1
     if (((sum_man & 0x1000000) >> 24) == 0x1) {
         sum_man >>= 1;
         sum_exp++;
+        if (sum_exp == 255) {
+            // Overflow: return +-inf
+            return (Binary32){ sign, 255, 0x0 };
+        }
     }
     while (((sum_man & 0x800000) >> 23) != 0x1) {
         sum_man <<= 1;
         sum_exp--;
+        if (sum_exp == 0) {
+            // Underflow, return a subnormal number
+            return (Binary32){ sign, 0, ((uint32_t)sum_man & 0x7fffff) };
+        }
     }
 
-    // (Operation for overflow/underflow of exp. bits should be done here)
-
-    // Signature bit
-    uint8_t sign = (sum_man & 0x80000000) >> 31;
+    // (Rounding by LSB should be done here)
+    // (Loop normalization if a rounded value is subnormal)
 
     return (Binary32){ sign, sum_exp, (uint32_t)(sum_man & 0x7fffff) };
 }
